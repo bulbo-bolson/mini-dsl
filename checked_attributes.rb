022588914556e10eb3684701794cf2c87e07249e
Bulbo <bolsonbulbo@gmail.com>
2019-12-31 18:42:33 +0100

    multiple attributes

--- checked_attributes.rb
+++ checked_attributes.rb
@@ -1,18 +1,22 @@
 
 module CheckedAttributes
 
-  def attr_checked(klass, attr, &validation)
+  def attr_checked(klass, *attrs, &validation)
 
     klass.class_eval do
-      # generate getter
-      define_method attr do
-        instance_variable_get "@#{attr}"
+
+      attrs.each do |attr|
+        # generate getter
+        define_method attr do
+          instance_variable_get "@#{attr}"
+        end
+        # generate setter
+        define_method "#{attr}=" do |value|
+          raise RuntimeError.new("Invalid value") unless validation.call(value)
+          instance_variable_set("@#{attr}", value)
+        end
       end
-      # generate setter
-      define_method "#{attr}=" do |value|
-        raise RuntimeError.new("Invalid value") unless validation.call(value)
-        instance_variable_set("@#{attr}", value)
-      end
+
     end
   end
 
--- spec/checked_attributes_spec.rb
+++ spec/checked_attributes_spec.rb
@@ -5,33 +5,64 @@ describe 'CheckedAttributes' do
 
   describe '#attr_checked' do
 
-    context 'with a restriction of older than 18' do
-      context 'with 8 years' do
-        it 'raises a RuntimeError' do
-          class Person
-            include CheckedAttributes
+    context 'with a single attribute' do
+      context 'with a restriction of older than 18' do
+        context 'with 8 years' do
+          it 'raises a RuntimeError' do
+            class Person
+              include CheckedAttributes
+            end
+            @bob = Person.new
+            @bob.attr_checked(Person, :age) do |value|
+              value >= 18
+            end
+            expect{@bob.age = 8}.to raise_error(RuntimeError, "Invalid value")
           end
-          @bob = Person.new
-          @bob.attr_checked(Person, "age") do |value|
-            value >= 18
+        end
+        context 'with 20 years' do
+          it 'accepts the value' do
+            class Person
+              include CheckedAttributes
+            end
+            @bob = Person.new
+            @bob.attr_checked(Person, :age) do |value|
+              value >= 18
+            end
+            @bob.age = 20
+            expect(@bob.age).to eq 20
           end
-          expect{@bob.age = 8}.to raise_error(RuntimeError, "Invalid value")
         end
       end
-      context 'with 20 years' do
-        it 'accepts the value' do
-          class Person
-            include CheckedAttributes
-          end
-          @bob = Person.new
-          @bob.attr_checked(Person, "age") do |value|
-            value >= 18
-          end
-          @bob.age = 20
-          expect(@bob.age).to eq 20
+
+    end
+
+    context 'with multiple attributes' do
+      it 'accepts the values' do
+        class Person
+          include CheckedAttributes
         end
+        @bob = Person.new
+        @bob.attr_checked(Person, :age, :gender) do |value|
+          !value.nil?
+        end
+        @bob.age = 20
+        @bob.gender = "male"
+        expect(@bob.age).to eq 20
+        expect(@bob.gender).to eq "male"
+      end
+      it 'enforces the block condition' do
+        class Person
+          include CheckedAttributes
+        end
+        @bob = Person.new
+        @bob.attr_checked(Person, :age, :gender) do |value|
+          !value.nil?
+        end
+        @bob.age = 20
+        expect{@bob.gender = nil}.to raise_error(RuntimeError, "Invalid value")
       end
     end
   end
+
 end
 
